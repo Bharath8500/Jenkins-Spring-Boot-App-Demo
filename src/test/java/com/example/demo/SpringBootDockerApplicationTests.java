383505f5c6f2427a21867df2716a027fe07daeb3
ChavalaBharath3 <bharathchavala396@gmail.com>
2024-06-20 14:49:11 -0500

    This is secound commit

--- src/test/java/com/example/demo/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootDockerApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDockerApplicationTests {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootDockerApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		log.debug("Hy this is running from test case centextLoads");
 	}
 
 }
